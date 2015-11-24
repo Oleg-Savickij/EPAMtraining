001337307f94437992a8cc195f202817a93ba5b4
Олег <katexizis94@gmail.com>
2015-11-25 02:52:27 +0300

    Create Model and DAL.

---
+++ EPAM laba 4/BL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+        private ProductRepository _products = new ProductRepository();
+        public void add(Product item)
+        {
+            _products.Add(item);
+        }
+    }
+}
